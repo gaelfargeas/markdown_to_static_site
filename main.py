4a64dfdb4b9e7f4355f7e332373220688657352d
gael fargeas <gael.fargeas@neuf.fr>
2019-01-02 23:40:17 +0100

    implementation : image

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import argparse
 from pathlib import Path
 import markdown2
 import jinja2
+import os
+import shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", help="Chemin du/des source.", type=str)
@@ -23,6 +25,54 @@ if VERBOSE:
     print("output :", args.o)
     print("template:", args.t)
     print("type input", args.s)
+
+
+def add_image(html_test):
+    # dans le output file crée un dossier src
+    src_path = Path(args.o + "/src")
+    if not os.path.exists(src_path):
+        os.makedirs(src_path)
+
+    # get le path du dossier input
+    if args.s:
+        input_path = os.path(args.i)
+    else:
+        input_path = os.path.dirname(args.i)
+
+    # parse le html , cp chaque image dans scr et modif le link
+    result_string = ""
+    for line in html_test.split("\n"):
+
+        line = str(line)
+        if "<img " in line:
+            # get image
+            image_path = line.split('src="')[1].split('" ')[0]
+            if VERBOSE:
+                print("image_path", image_path)
+
+            image_name = image_path.split("/")[-1].split("\\")[-1]
+            if VERBOSE:
+                print("image_name", image_name)
+
+            shutil.copyfile(
+                str(input_path) + "/" + image_path, str(src_path) + "/" + image_name
+            )
+
+            line = (
+                line.split('src="')[0]
+                + 'src="./src/'
+                + image_name
+                + '" '
+                + line.split('src="')[-1].split('" ')[-1]
+            )
+            result_string += line + "\n"
+
+        else:
+            result_string += line + "\n"
+
+    return result_string
+
+
 if __name__ == "__main__":
     if args.s:
         if args.i != None and args.o != None:
@@ -134,5 +184,6 @@ if __name__ == "__main__":
                     if VERBOSE:
                         print("template file :", args.t)
 
+                    resutl = add_image(resutl)
                     output_file.write(resutl)
 
